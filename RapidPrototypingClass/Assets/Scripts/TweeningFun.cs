4f4063ed16940f7cc2602ea97ed8e6e46b2bd1e3
Ryan <Rmurphy1@myjmc.edu.au>
2018-09-26 16:08:30 +1000

    Class1
    First Class, tween test.

---
+++ RapidPrototypingClass/Assets/Scripts/TweeningFun.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class TweeningFun : MonoBehaviour
+{
+    public GameObject cube;
+    public float tweenSpeed = 2f;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(Input.GetKeyDown(KeyCode.A))
+        {
+            cube.transform.DOScale(4, tweenSpeed);
+        }
+
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+            cube.transform.DOMoveX(10, tweenSpeed);
+        }
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            cube.transform.DOPunchPosition(Vector3.forward * 5, tweenSpeed, 1, 0.1f);
+        }
+
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            Camera.main.DOShakePosition(tweenSpeed, 3, 10, 90);
+        }
+
+	}
+}
